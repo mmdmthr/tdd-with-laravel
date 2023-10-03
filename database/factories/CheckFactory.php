91d5d61b8f3fdf107eb234a7b4fa4448870f9169
mmdmthr <muhhari@gmail.com>
2023-10-03 20:44:11 +0700

    1. setting up application

---
+++ database/factories/CheckFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CheckFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'response_status' => rand(200, 204),
+            'response_content' => $this->faker->randomHtml(),
+            'elapsed_time' => random_int(1, 100),
+        ];
+    }
+}
---
+++ database/migrations/2023_10_03_132243_create_checks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChecksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('response_status');
+            $table->text('response_content');
+            $table->integer('elapsed_time');
+            $table->unsignedInteger('site_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checks');
+    }
+}
