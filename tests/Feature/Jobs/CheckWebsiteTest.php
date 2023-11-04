ae7469bbfec99bc52fbcfaf73d02add87d8eafc4
mmdmthr <muhhari@gmail.com>
2023-11-04 21:32:10 +0700

    10 - faking the HTTP client

--- tests/Feature/Jobs/CheckWebsiteTest.php
+++ tests/Feature/Jobs/CheckWebsiteTest.php
@@ -7,6 +7,7 @@ use App\Models\Site;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class CheckWebsiteTest extends TestCase
@@ -17,7 +18,14 @@ class CheckWebsiteTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $site = $user->sites()->save(Site::factory()->make());
+        $site = $user->sites()->save(Site::factory()->make([
+                'url' => 'https://google.com',
+        ]));
+
+        Http::fake(function($request) {
+            usleep(200 * 1000);
+            return Http::response('<h1>Success</h1>', 200);
+        });
 
         $this->assertEquals(0, $site->checks()->count());
 
@@ -27,8 +35,8 @@ class CheckWebsiteTest extends TestCase
         $site->refresh();
         $check = $site->checks()->first();
         $this->assertEquals(200, $check->response_status);
-        $this->assertNotNull($check->response_content);
-        $this->assertTrue($check->elapsed_time > 1);
+        $this->assertEquals('<h1>Success</h1>', $check->response_content);
+        $this->assertTrue($check->elapsed_time >= 200);
         $this->assertTrue($site->is_online);
         dd($check->elapsed_time);
     }
